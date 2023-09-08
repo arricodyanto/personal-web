beed3789af12a61b76d442325b21d09a6d624004
arricodyanto <arricohandyanto@gmail.com>
2023-09-09 00:45:51 +0700

    Fix responsiveness in each devices

--- src/common/components/molecules/ExperienceTimeline.tsx
+++ src/common/components/molecules/ExperienceTimeline.tsx
@@ -7,7 +7,8 @@ import {
 	TimelineOppositeContent,
 	TimelineSeparator,
 } from '@mui/lab';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export default function ExperienceTimeline() {
 	return (
@@ -15,12 +16,18 @@ export default function ExperienceTimeline() {
 			position='right'
 			className='mt-8'>
 			<TimelineItem>
-				<TimelineOppositeContent color='text.secondary'>
-					<Typography
-						variant='subtitle1'
-						className='font-bold text-lg'>
-						Universitas Sebelas Maret Public Relation, Surakarta
-					</Typography>
+				<TimelineOppositeContent
+					color='text.secondary'
+					display={{ xs: 'none', md: 'block' }}>
+					<Link
+						to='https://uns.ac.id/'
+						target='blank'>
+						<Typography
+							variant='subtitle1'
+							className='font-bold text-lg hover:brightness-125 hover:underline hover:underline-offset-2'>
+							Universitas Sebelas Maret Public Relation, Surakarta
+						</Typography>
+					</Link>
 					<Typography
 						variant='subtitle2'
 						className=''>
@@ -37,7 +44,7 @@ export default function ExperienceTimeline() {
 				<TimelineContent>
 					<Typography
 						variant='body1'
-						className='font-bold text-text-secondary text-lg'>
+						className='font-bold text-text-secondary xs:text-xl md:text-lg'>
 						Web Administrator
 					</Typography>
 					<Typography
@@ -50,15 +57,37 @@ export default function ExperienceTimeline() {
 						various office equipment. This was part of a student internship
 						program offered by the university.
 					</Typography>
+					<Box display={{ xs: 'block', md: 'none' }}>
+						<Link
+							to='https://uns.ac.id/'
+							target='blank'>
+							<Typography
+								variant='subtitle1'
+								className='font-bold text-lg hover:brightness-125 hover:underline hover:underline-offset-2 text-text-secondary leading-6 mt-2'>
+								Universitas Sebelas Maret Public Relation, Surakarta
+							</Typography>
+						</Link>
+						<Typography
+							variant='subtitle2'
+							className=''>
+							February 2021 - December 2022
+						</Typography>
+					</Box>
 				</TimelineContent>
 			</TimelineItem>
 			<TimelineItem>
-				<TimelineOppositeContent color='text.secondary'>
-					<Typography
-						variant='subtitle1'
-						className='font-bold text-lg'>
-						Refactory, Yogyakarta
-					</Typography>
+				<TimelineOppositeContent
+					color='text.secondary'
+					display={{ xs: 'none', md: 'block' }}>
+					<Link
+						to='https://refactory.id/'
+						target='blank'>
+						<Typography
+							variant='subtitle1'
+							className='font-bold text-lg hover:brightness-125 hover:underline hover:underline-offset-2'>
+							Refactory, Yogyakarta
+						</Typography>
+					</Link>
 					<Typography
 						variant='subtitle2'
 						className=''>
@@ -74,7 +103,7 @@ export default function ExperienceTimeline() {
 				<TimelineContent>
 					<Typography
 						variant='body1'
-						className='font-bold text-text-secondary text-lg'>
+						className='font-bold text-text-secondary xs:text-xl md:text-lg'>
 						Back End Developer
 					</Typography>
 					<Typography
@@ -86,6 +115,22 @@ export default function ExperienceTimeline() {
 						University and Refactory Yogyakarta. In this project, I created a
 						Rest API using Express JS and PostgreSQL, completed in Q4 2021.
 					</Typography>
+					<Box display={{ xs: 'block', md: 'none' }}>
+						<Link
+							to='https://refactory.id/'
+							target='blank'>
+							<Typography
+								variant='subtitle1'
+								className='font-bold text-lg hover:brightness-125 hover:underline hover:underline-offset-2 text-text-secondary leading-6 mt-2'>
+								Refactory, Yogyakarta
+							</Typography>
+						</Link>
+						<Typography
+							variant='subtitle2'
+							className=''>
+							August 2021 - December 2021
+						</Typography>
+					</Box>
 				</TimelineContent>
 			</TimelineItem>
 		</Timeline>
